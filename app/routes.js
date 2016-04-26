6d5b962368c7c4e780646fdace1af5f6cf8c366f
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2016-04-26 15:25:24 +0100

    applicant details page

--- app/routes.js
+++ app/routes.js
@@ -2,51 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-
   res.render('index');
-
 });
 
 router.get('/dashboard', function (req, res) {
-
   res.render('dashboard');
-
 });
 
 router.get('/full-application', function (req, res) {
-
   res.render('full-application');
-
 });
 
 router.get('/applicant/:page', function (req, res) {
-
   res.render('/applicant/' + req.params.page);
-
 });
 
 router.get('/financial/:page', function (req, res) {
-
   res.render('/financial/' + req.params.page);
-
 });
 
 router.get('/project/:page', function (req, res) {
-
   res.render('/project/' + req.params.page);
-
 });
 
 router.get('/project-partners/:page', function (req, res) {
-
   res.render('/project_partners/' + req.params.page);
-
 });
 
 router.get('/strategy/:page', function (req, res) {
-
   res.render('/strategy/' + req.params.page);
-
 });
 
 module.exports = router;
