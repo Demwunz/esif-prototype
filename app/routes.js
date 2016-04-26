ac8d84789b7254c679488ed41cbd0890cc525579
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2016-04-26 17:06:46 +0100

    amended project routes

--- app/routes.js
+++ app/routes.js
@@ -13,24 +13,24 @@ router.get('/full-application', function (req, res) {
   res.render('full-application');
 });
 
-router.get('/applicant/:page', function (req, res) {
-  res.render('/applicant/' + req.params.page);
-});
+// router.get('/applicant/:page', function (req, res) {
+//   res.render('/applicant/' + req.params.page);
+// });
 
-router.get('/financial/:page', function (req, res) {
-  res.render('/financial/' + req.params.page);
-});
+// router.get('/financial/:page', function (req, res) {
+//   res.render('/financial/' + req.params.page);
+// });
 
-router.get('/project/:page', function (req, res) {
-  res.render('/project/' + req.params.page);
-});
+// router.get('/project/:page', function (req, res) {
+//   res.render('/project/' + req.params.page);
+// });
 
-router.get('/project-partners/:page', function (req, res) {
-  res.render('/project_partners/' + req.params.page);
-});
-
-router.get('/strategy/:page', function (req, res) {
-  res.render('/strategy/' + req.params.page);
-});
+// router.get('/project-partners/:page', function (req, res) {
+//   res.render('/project_partners/' + req.params.page);
+// });
+//
+// router.get('/strategy/:page', function (req, res) {
+//   res.render('/strategy/' + req.params.page);
+// });
 
 module.exports = router;
